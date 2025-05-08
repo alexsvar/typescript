bdcc319d24a86540ef4c3c927d2b0c5cec6c34c2
Alexander Morozov <27691062+alexsvar@users.noreply.github.com>
2025-05-08 16:13:20 +0300

    Exercise 1: type an object

--- app.ts
+++ app.ts
@@ -39,3 +39,39 @@ const user = {
   }
 };
 // console.log(getName(user)); // Alex Svar
+
+// EXERCISE 1:
+// type object:
+// {
+//   "officeId": 45,
+//   "isOpened": false,
+//   "contacts": {
+//     "phone": "+79100000000",
+//     "email": "my@mail.ru",
+//     "address": {
+//       "city": "Moscow"
+//     }
+//   }
+// }
+
+let info: {
+  officeId: number;
+  isOpened: boolean;
+  contacts: {
+    phone: string;
+    email: string;
+    address: {
+      city: string;
+    };
+  };
+} = {
+  officeId: 45,
+  isOpened: false,
+  contacts: {
+    phone: '+79100000000',
+    email: 'my@mail.ru',
+    address: {
+      city: 'Moscow'
+    }
+  }
+};
