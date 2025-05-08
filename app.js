4332ec03190fe369da2d386c4783e1c90c773ce2
Alexander Morozov <27691062+alexsvar@users.noreply.github.com>
2025-05-08 12:04:26 +0300

    Number, string, boolean

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+// The Main Types
+let revenue = 10000;
+let bonus = 500;
+let result = revenue + bonus;
+let c = 'String!';
+let d = true;
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,12 @@
+// THE MAIN TYPES
+// number:
+let revenue: number = 10_000;
+let bonus: number = 500;
+let result: number = revenue + bonus;
+
+// string
+let c: string = 'String!';
+
+// boolean:
+let d: boolean = true;
+console.log(result);
