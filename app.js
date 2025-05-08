c0f1cba6a05e7f55caf8c5df6b22d5949568ba19
Alexander Morozov <27691062+alexsvar@users.noreply.github.com>
2025-05-08 15:50:35 +0300

    Types in functions

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 "use strict";
-// The Main Types
+// THE MAIN TYPES
+// number:
 let revenue = 10000;
 let bonus = 500;
 let result = revenue + bonus;
+// string:
 let c = 'String!';
+// boolean:
 let d = true;
-console.log(result);
+// console.log(result);
+// types in functions:
+// tsconfig.json:
+// "noImplicitAny": true, => "noImplicitAny": false,
+//  Enable error reporting for expressions and declarations with an implied 'any' type.
+function getFullName(firstname, surname) {
+    return `${firstname} ${surname}`;
+}
+const getFullNameArrow = (firstname, surname) => {
+    return `${firstname} ${surname}`;
+};
+console.log(getFullName('Alex', 'Svar'));
--- app.ts
+++ app.ts
@@ -4,9 +4,22 @@ let revenue: number = 10_000;
 let bonus: number = 500;
 let result: number = revenue + bonus;
 
-// string
+// string:
 let c: string = 'String!';
 
 // boolean:
 let d: boolean = true;
-console.log(result);
+// console.log(result);
+
+// types in functions:
+// tsconfig.json:
+// "noImplicitAny": true, => "noImplicitAny": false,
+//  Enable error reporting for expressions and declarations with an implied 'any' type.
+function getFullName(firstname: string, surname: string): string {
+  return `${firstname} ${surname}`;
+}
+
+const getFullNameArrow = (firstname: string, surname: string): string => {
+  return `${firstname} ${surname}`;
+};
+// console.log(getFullName('Alex', 'Svar'));
