f29a4ca6676387ca1749cb0ff7f1b14869a7e363
Alexander Morozov <27691062+alexsvar@users.noreply.github.com>
2025-05-10 10:19:43 +0300

    Types or interfaces

--- advanced_types.ts
+++ advanced_types.ts
@@ -1,159 +1,192 @@
-// ADVANCED TYPES
-// UNION
-// Union используется для возможности обозначить, что в той или иной переменной у нас
-// могут находиться различные типы при различных обстоятельствах.
-// const arr2: (string | number)[] = ['str', 1];
-
-// string, number, boolean:
-function logId2(id: string | number | boolean) {
-  // Cужение типов для union types:
-  if (typeof id === 'string') {
-    console.log(id);
-  } else if (typeof id === 'number') {
-    console.log(id);
-  } else {
-    console.log(id);
-  }
-}
-logId2(1);
-logId2('str');
-logId2(true);
-
-// arrays:
-function logError2(err: string | string[]) {
-  if (Array.isArray(err)) {
-    console.log(err);
-  } else {
-    console.log(err);
-  }
-}
-
-// objects:
-function logObject2(obj: { a: number } | { b: number }) {
-  if ('a' in obj) {
-    console.log(obj.a);
-  } else {
-    console.log(obj.b);
-  }
-}
-
-// parameters with similar types:
-function logMultipleIds2(a: string | number, b: string | boolean) {
-  if (a === b) {
-  } else {
-    console.log(a);
-  }
-}
-
-// LITERAL
-// Можно сделать enum
-// enum RequestType {
-//   GET = 'get',
-//   POST = 'post'
-// }
-// enum делать для каждого такого случая является плохой практикой.
+// // ADVANCED TYPES
+// // UNION
+// // Union используется для возможности обозначить,
+// // что в той или иной переменной у нас могут находиться
+// // различные типы при различных обстоятельствах.
+// // const arr: (string | number)[] = ['str', 1];
 //
-// string literal types:
-function fetchWithAuth2(url: string, method: 'post' | 'get'): 1 | -1 {
-  return 1;
-}
-
-// Когда объявляем константу const a2 = 'str', то у a будет тип 'str'.
-// В таком виде тип бесполезен, мы ничего другого присвоить не можем.
-const a2 = 'str';
-
-// Если мы объявим переменную b2 let b2 = 'asdasd', то тип будет 'string'.
-// Но мы можем чётко написать let b2: 'asdasd' = 'asdasd' и b2 становится константой.
-let b2: 'asdasd' = 'asdasd';
-
-// На фронте используется в React для задания props
-fetchWithAuth2('http://asd.asd', 'get');
-
-// Если явно не указать тип 'post', то будет ошибка, потому что идёт проверка на тип.
-// let method: 'post' = 'post';
-// Или можем сделать method константой.
-// const method = 'post';
-// Или же можем сделать каст к другому типу.
-// Но если в переменную мы передадим что-то другое, то типизация не покажет ошибку.
-let method = 'post';
-fetchWithAuth2('http://asd.asd', method as 'post');
-
-// ALIASES
-// Позволяет более эффективно записывать типы.
-// Объявляется с помощью ключевого слова httpMethod.
-type httpMethod = 'post' | 'get';
-function fetchWithAuth3(url: string, method: httpMethod): 1 | -1 {
-  return 1;
-}
-
-type User2 = {
-  name: string;
-  age: number;
-  skills: string[];
-};
-type Role2 = {
-  name: string;
-  id: number;
-};
+// // string, number, boolean:
+// function logId(id: string | number | boolean) {
+//   // Cужение типов для union types:
+//   if (typeof id === 'string') {
+//     console.log(id);
+//   } else if (typeof id === 'number') {
+//     console.log(id);
+//   } else {
+//     console.log(id);
+//   }
+// }
+// logId(1);
+// logId('str');
+// logId(true);
+//
+// // arrays:
+// function logError(err: string | string[]) {
+//   if (Array.isArray(err)) {
+//     console.log(err);
+//   } else {
+//     console.log(err);
+//   }
+// }
+//
+// // objects:
+// function logObject(obj: { a: number } | { b: number }) {
+//   if ('a' in obj) {
+//     console.log(obj.a);
+//   } else {
+//     console.log(obj.b);
+//   }
+// }
+//
+// // parameters with similar types:
+// function logMultipleIds(a: string | number, b: string | boolean) {
+//   if (a === b) {
+//   } else {
+//     console.log(a);
+//   }
+// }
+//
+// // LITERAL
+// // Можно сделать enum
+// // enum RequestType {
+// //   GET = 'get',
+// //   POST = 'post'
+// // }
+// // enum делать для каждого такого случая является плохой практикой.
+// //
+// // string literal types:
+// function fetchWithAuth(url: string, method: 'post' | 'get'): 1 | -1 {
+//   return 1;
+// }
+//
+// // Когда объявляем константу const a = 'str', то у a будет тип 'str'.
+// // В таком виде тип бесполезен, мы ничего другого присвоить не можем.
+// const a = 'str';
+//
+// // Если мы объявим переменную b let b = 'asdasd', то тип будет 'string'.
+// // Но мы можем чётко написать let b: 'asdasd' = 'asdasd'
+// // и b становится константой.
+// let b: 'asdasd' = 'asdasd';
+//
+// // На фронте используется в React для задания props
+// fetchWithAuth('http://asd.asd', 'get');
+//
+// // Если явно не указать тип 'post', то будет ошибка, потому что идёт проверка на тип.
+// // let method: 'post' = 'post';
+// // Или можем сделать method константой.
+// // const method = 'post';
+// // Или же можем сделать каст к другому типу.
+// // Но если в переменную мы передадим что-то другое, то типизация не покажет ошибку.
+// let method = 'post';
+// fetchWithAuth('http://asd.asd', method as 'post');
+//
+// // ALIASES (TYPES)
+// // Позволяет более эффективно записывать типы.
+// // Объявляется с помощью ключевого слова httpMethod.
+// type httpMethod = 'post' | 'get';
+// function fetchWithAuth(url: string, method: httpMethod): 1 | -1 {
+//   return 1;
+// }
+//
+// type User = {
+//   name: string;
+//   age: number;
+//   skills: string[];
+// };
+// type Role = {
+//   name: string;
+//   id: number;
+// };
 // Можно создать intersection User2WithRole2, который будет типа User2 & Role2.
 // Через "|" мы обозначаем union (или 1 или 2 ...).
 // Через "&" мы обозначаем intersection (объединение, и 1 и 2).
-type User2WithRole2 = User2 & Role2;
-let user2: User2WithRole2 = {
-  name: 'Alex',
-  age: 36,
-  skills: ['1', '2'],
-  id: 1
-};
-
+// type UserWithRole = User & Role;
+// let user: UserWithRole = {
+//   name: 'Alex',
+//   age: 36,
+//   skills: ['1', '2'],
+//   id: 1
+// };
+//
 // INTERFACES
-interface User {
-  name: string;
-  age: number;
-  skills: string[];
-}
-interface Role {
-  roleId: number;
-}
-interface UserWithRole extends User, Role {
-  createdAt: Date;
-}
-let user: UserWithRole = {
-  name: 'Alex',
-  age: 36,
-  skills: ['1', '2'],
-  roleId: 1,
-  createdAt: new Date()
-};
-
+// interface User {
+//   name: string;
+//   age: number;
+//   skills: string[];
+// }
+// interface Role {
+//   roleId: number;
+// }
+// interface UserWithRole extends User, Role {
+//   createdAt: Date;
+// }
+// let user: UserWithRole = {
+//   name: 'Alex',
+//   age: 36,
+//   skills: ['1', '2'],
+//   roleId: 1,
+//   createdAt: new Date()
+// };
+//
 // functions:
 // function log in interface User1
-interface User1 {
+// interface User {
+//   name: string;
+//   age: number;
+//   skills: string[];
+//
+//   log: (id: number) => string;
+// }
+// interface Role {
+//   roleId: number;
+// }
+//
+// interface User1WithRole1 extends User, Role {
+//   createdAt: Date;
+// }
+// let user3: User1WithRole1 = {
+//   name: 'Alex',
+//   age: 36,
+//   skills: ['1', '2'],
+//   roleId: 1,
+//   createdAt: new Date(),
+//
+//   log(id) {
+//     return '';
+//   }
+// };
+//
+// interface UserDictionary {
+//   [index: number]: User;
+// }
+//
+// TYPES OR INTERFACES
+// interfaces:
+// Один interface может дополнять другой, но это bad practice.
+// Такой код плохо поддерживаемый. Но это может быть полезным
+// при использовании типов из какой-либо библиотеки.
+// С type так делать нельзя.
+interface User {
   name: string;
+}
+interface User {
   age: number;
-  skills: string[];
-
-  log: (id: number) => string;
 }
-interface Role1 {
-  roleId: number;
-}
-
-interface User1WithRole1 extends User1, Role1 {
-  createdAt: Date;
-}
-let user3: User1WithRole1 = {
+const user: User = {
   name: 'Alex',
-  age: 36,
-  skills: ['1', '2'],
-  roleId: 1,
-  createdAt: new Date(),
-
-  log(id) {
-    return '';
-  }
+  age: 33
 };
-
-interface UserDictionary {
-  [index: number]: User1;
+//
+// types:
+// Позволяют расширяться от примитивных типов.
+// С interfaces так нельзя. Interfaces позволяют
+// работать с объектами, с описанием классов, но не с простыми типами.
+// Только внутри interface могут содержаться простые типы.
+type ID = string | number;
+interface IDS {
+  ID: string | number;
 }
+//
+// Рекомендации к использванию:
+// types: Для использования примитивных типов.
+// interface: Для работы с объектами и описания классов.
--- main_types.ts
+++ main_types.ts
@@ -1,168 +1,193 @@
-// THE MAIN TYPES
-// NUMBER
-let revenue1: number = 10_000;
-let bonus1: number = 500;
-let result1: number = revenue1 + bonus1;
-
-// STRING
-let c1: string = 'String!';
-
-// BOOLEAN
-let d1: boolean = true;
-console.log(result1); // 10500
-
-// FUNCTIONS
-// tsconfig.json:
-// "noImplicitAny": true, => "noImplicitAny": false,
-//  Enable error reporting for expressions and declarations with an implied 'any' type.
-function getFullName1(firstname1: string, surname1: string): string {
-  return `${firstname1} ${surname1}`;
-}
-
-const getFullNameArrow1 = (firstname: string, surname: string): string => {
-  return `${firstname} ${surname}`;
-};
-console.log(getFullName1('Alex', 'Svar')); // Alex Svar
-
-// OBJECTS
-function getName1(userEntity: { firstname: string; surname: string }): string {
-  return `${userEntity.firstname} ${userEntity.surname}`;
-}
-const user1 = {
-  firstname: 'Alex',
-  surname: 'Svar',
-  age: 36,
-  city: 'Sarov',
-  skills: {
-    dev: true,
-    devops: false
-  }
-};
-console.log(getName1(user1)); // Alex Svar
-
-// EXERCISE 1
-// type object:
-// {
-//   "officeId": 45,
-//   "isOpened": false,
-//   "contacts": {
-//     "phone": "+79100000000",
-//     "email": "my@mail.ru",
-//     "address": {
-//       "city": "Moscow"
+// // THE MAIN TYPES
+// // NUMBER
+// let revenue: number = 10_000;
+// let bonus: number = 500;
+// let result: number = revenue + bonus;
+//
+// // STRING
+// let c: string = 'String!';
+// // BOOLEAN
+// let d: boolean = true;
+// console.log(result); // 10500
+//
+// // FUNCTIONS
+// // tsconfig.json:
+// // "noImplicitAny": true, => "noImplicitAny": false,
+// //  Enable error reporting for expressions and declarations with an implied 'any' type.
+// function getFullName(firstname: string, surname: string): string {
+//   return `${firstname} ${surname}`;
+// }
+//
+// const getFullNameArrow = (firstname: string, surname: string): string => {
+//   return `${firstname} ${surname}`;
+// };
+// console.log(getFullName('Alex', 'Svar')); // Alex Svar
+//
+// // OBJECTS
+// function getName(userEntity: { firstname: string; surname: string }): string {
+//   return `${userEntity.firstname} ${userEntity.surname}`;
+// }
+// const user = {
+//   firstname: 'Alex',
+//   surname: 'Svar',
+//   age: 36,
+//   city: 'Sarov',
+//   skills: {
+//     dev: true,
+//     devops: false
+//   }
+// };
+// console.log(getName(user)); // Alex Svar
+//
+// // EXERCISE 1
+// // type object:
+// // {
+// //   "officeId": 45,
+// //   "isOpened": false,
+// //   "contacts": {
+// //     "phone": "+79100000000",
+// //     "email": "my@mail.ru",
+// //     "address": {
+// //       "city": "Moscow"
+// //     }
+// //   }
+// // }
+//
+// let inf: {
+//   officeId: number;
+//   isOpened: boolean;
+//   contacts: {
+//     phone: string;
+//     email: string;
+//     address: {
+//       city: string;
+//     };
+//   };
+// } = {
+//   officeId: 45,
+//   isOpened: false,
+//   contacts: {
+//     phone: '+79100000000',
+//     email: 'my@mail.ru',
+//     address: {
+//       city: 'Moscow'
 //     }
 //   }
+// };
+//
+// // ARRAYS
+// const skills: string[] = ['Dev', 'DevOps', 'Testing'];
+//
+// // iteration
+// for (const skill of skills) {
+//   console.log(skill.toLocaleLowerCase()); // dev devops
 // }
-
-let info1: {
-  officeId: number;
-  isOpened: boolean;
-  contacts: {
-    phone: string;
-    email: string;
-    address: {
-      city: string;
-    };
-  };
-} = {
-  officeId: 45,
-  isOpened: false,
-  contacts: {
-    phone: '+79100000000',
-    email: 'my@mail.ru',
-    address: {
-      city: 'Moscow'
-    }
-  }
-};
-
-// ARRAYS
-const skills1: string[] = ['Dev', 'DevOps', 'Testing'];
-
-// iteration
-for (const skill of skills1) {
-  console.log(skill.toLocaleLowerCase()); // dev devops
-}
-
-const res = skills1
-  .filter((skill: string) => skill !== 'DevOps')
-  .map((skill) => skill + '! ')
-  .reduce((a, b) => a + b);
-console.log(res);
-
-// TUPLES
-const skill1: [number, string] = [1, 'dev'];
-const [skill1Id, skill1Name] = skill1;
-// const q = skill[2]; // ERROR
-
-const arr1: [number, string, ...boolean[]] = [1, 'str', true, true, false];
-
-// READONLY
-// tuple:
-const hobby1: readonly [number, string] = [1, 'music'];
-// hobby[0] = 2; // ERROR
-
-// array:
-const nums1: ReadonlyArray<number> = [1, 2, 3, 4, 5];
-// nums[1] = 10; // ERROR
-
-// ENUMS
-// Применяются для списоков ответов, значений и параметров с ограниченным числом значений
-
-// statusCodes:
-// 1 - success,
-// 2 - in process,
-// 3 - failed
-
-// heterogeneous enum:
-enum StatusCode1 {
-  SUCCESS = 1,
-  IN_PROGRESS = 'p',
-  FAILED = 'f'
-}
-
-const mainResult1 = {
-  message: 'The payment was successful',
-  statusCode: StatusCode1.SUCCESS
-};
-
-function action(status: StatusCode1) {}
-action(StatusCode1.SUCCESS);
-action(1);
-// action('p'); // ERROR
-
-// constant enum
-const enum Roles1 {
-  ADMIN = 1,
-  USER = 2,
-  PREMIUM_USER = 3
-}
-const enumResult = Roles1.ADMIN;
-
-// EXERCISE 2
-// type a function:
-// request:
-// {
-// 	"topicId": 5,
-// 	"status": "published" // "draft", "deleted"
+//
+// const res = skills
+//   .filter((skill: string) => skill !== 'DevOps')
+//   .map((skill) => skill + '! ')
+//   .reduce((a, b) => a + b);
+// console.log(res);
+//
+// // TUPLES
+// const skill: [number, string] = [1, 'dev'];
+// const [skillId, skillName] = skill;
+// // const q = skill[2]; // ERROR
+//
+// const arr: [number, string, ...boolean[]] = [1, 'str', true, true, false];
+//
+// // READONLY
+// // tuple:
+// const hobby: readonly [number, string] = [1, 'music'];
+// // hobby[0] = 2; // ERROR
+//
+// // array:
+// const nums: ReadonlyArray<number> = [1, 2, 3, 4, 5];
+// // nums[1] = 10; // ERROR
+//
+// // ENUMS
+// // Применяются для списоков ответов, значений и параметров с ограниченным числом значений
+//
+// // statusCodes:
+// // 1 - success,
+// // 2 - in process,
+// // 3 - failed
+//
+// // heterogeneous enum:
+// enum StatusCode {
+//   SUCCESS = 1,
+//   IN_PROGRESS = 'p',
+//   FAILED = 'f'
 // }
-
-// response:
-// [
-// 	{
-// 		"question": "Как осуществляется доставка?",
-// 		"answer": "Быстро!",
-// 		"tags": [
-// 			"popular",
-// 			"new"
-// 		],
-// 		"likes": 3,
-// 		"status": "publishes"
-// 	}
-// ]
-
-// function:
-// async function getFaqs(req) {
+//
+// const mainResult = {
+//   message: 'The payment was successful',
+//   statusCode: StatusCode.SUCCESS
+// };
+//
+// function action(status: StatusCode) {}
+// action(StatusCode.SUCCESS);
+// action(1);
+// // action('p'); // ERROR
+//
+// // constant enum
+// const enum Roles {
+//   ADMIN = 1,
+//   USER = 2,
+//   PREMIUM_USER = 3
+// }
+// const enumResult = Roles.ADMIN;
+//
+// // EXERCISE 2
+// // type a function:
+// // request:
+// // {
+// // 	"topicId": 5,
+// // 	"status": "published" // "draft", "deleted"
+// // }
+//
+// // response:
+// // [
+// // 	{
+// // 		"question": "Как осуществляется доставка?",
+// // 		"answer": "Быстро!",
+// // 		"tags": [
+// // 			"popular",
+// // 			"new"
+// // 		],
+// // 		"likes": 3,
+// // 		"status": "publishes"
+// // 	}
+// // ]
+//
+// // function:
+// // async function getFaqs(req) {
+// //   const res = await fetch('/faqs', {
+// //     method: 'POST',
+// //     body: JSON.stringify(req)
+// //   });
+// //   const data = await res.json();
+// //   return data;
+// // }
+//
+// enum QuestionStatus {
+//   Published = 'published',
+//   Draft = 'draft',
+//   Deleted = 'deleted'
+// }
+//
+// async function getFaqs(req: {
+//   topicId: number;
+//   status?: QuestionStatus;
+// }): Promise<
+//   {
+//     question: string;
+//     answer: string;
+//     tags: string[];
+//     likes: number;
+//     status: QuestionStatus;
+//   }[]
+// > {
 //   const res = await fetch('/faqs', {
 //     method: 'POST',
 //     body: JSON.stringify(req)
@@ -170,29 +195,3 @@ const enumResult = Roles1.ADMIN;
 //   const data = await res.json();
 //   return data;
 // }
-
-enum QuestionStatus1 {
-  Published = 'published',
-  Draft = 'draft',
-  Deleted = 'deleted'
-}
-
-async function getFaqs1(req: {
-  topicId: number;
-  status?: QuestionStatus1;
-}): Promise<
-  {
-    question: string;
-    answer: string;
-    tags: string[];
-    likes: number;
-    status: QuestionStatus1;
-  }[]
-> {
-  const res1 = await fetch('/faqs', {
-    method: 'POST',
-    body: JSON.stringify(req)
-  });
-  const data1 = await res1.json();
-  return data1;
-}
