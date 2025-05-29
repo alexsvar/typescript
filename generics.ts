28dbb3318bd0ac6cd77c857117de2c648ecedc90
Alexander Morozov <27691062+alexsvar@users.noreply.github.com>
2025-05-29 11:44:00 +0300

    Generics in functions

--- generics.ts
+++ generics.ts
@@ -1,19 +1,37 @@
-// GENERICS
-// GENERICS
-// Generics - обобщённый тип, позволяющий резервировать место для типа,
-// который будет при вызове заменён на определённый.
+// // GENERICS
+// // GENERICS
+// // Generics - обобщённый тип, позволяющий резервировать место для типа,
+// // который будет при вызове заменён на определённый.
 //
-// EMBEDDED GENERICS
+// // EMBEDDED GENERICS
 //
-const nums: Array<number> = [1, 2, 3];
-
-async function test() {
-  const a = await new Promise<number>((resolve, reject) => {
-    resolve(1);
-  });
+// const nums: Array<number> = [1, 2, 3];
+//
+// async function test() {
+//   const a = await new Promise<number>((resolve, reject) => {
+//     resolve(1);
+//   });
+// }
+//
+// const check: Record<string, boolean> = {
+//   drive: true,
+//   kpp: false
+// };
+//
+// GENERICS IN FUNCTIONS
+// Generics в функции принято называть либо одной большой буквой,
+// либо названием с какой-либо смысловой нагрузкой.
+function logMiddleware<T>(data: T): T {
+  console.log(data);
+  return data;
 }
 
-const check: Record<string, boolean> = {
-  drive: true,
-  kpp: false
-};
+const res1 = logMiddleware<number>(10);
+const res2 = logMiddleware<string>('asdf');
+
+function getSplittedHalf<T>(data: Array<T>): Array<T> {
+  const l = data.length / 2;
+  return data.splice(0, l);
+}
+
+getSplittedHalf<number>([1, 3, 4]);
